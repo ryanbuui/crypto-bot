951d039f02407d74b906b4ba8943d3b4fe74647d
ryanbuui <57205440+ryanbuui@users.noreply.github.com>
2021-03-27 16:47:42 -0700

    Updated repo structure and created example files

---
+++ client/src/components/ExampleComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExampleComp() {
+  return (
+    <div>
+      Test
+    </div>
+  )
+}
+
+export default ExampleComp;
---
+++ client/src/pages/ExamplePage/Example.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ExampleComp from "../../components/ExampleComp";
+
+class Example extends Component {
+  render() {
+    return (
+      <div>
+        <ExampleComp />
+      </div>
+    )
+  }
+}
+
+export default Example;
